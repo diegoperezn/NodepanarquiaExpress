4103d21a863419708c8fdc1f75f18c6f7aa3e87c
diego.nadler <diego.nadler@foodstuffs.co.nz>
2024-12-09 22:42:36 -0300

    deploy render

--- Controller/CategoryController.js
+++ Controller/CategoryController.js
@@ -5,73 +5,77 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res){
-	
+app.get('/test', function (req, res) {
+	res.send("Success");
+});
+
+app.get('/', function (req, res) {
+
 	categoryService.findAll(function (err, list) {
 		if (err) {
-			res.send(err); 
-		} else{
-			res.send(list); 
-		}				
+			res.send(err);
+		} else {
+			res.send(list);
+		}
 	});
 });
 
-app.get('/:id', function(req, res){
+app.get('/:id', function (req, res) {
 	var id = req.params.id;
 
 	categoryService.findById(id, function (err, list) {
 		if (err) {
-			res.send(err); 
-		} else{
-			res.send(list); 
-		}				
+			res.send(err);
+		} else {
+			res.send(list);
+		}
 	});
 });
 
-app.delete('/:id', function(req, res){
+app.delete('/:id', function (req, res) {
 	var id = req.params.id;
 
 	categoryService.remove(id, function (err) {
 		if (err) {
-			res.send(err); 
-		} else{
-			res.send("Removed :)"); 
-		}				
+			res.send(err);
+		} else {
+			res.send("Removed :)");
+		}
 	});
 });
 
-app.post('/:id/update', function(req, res){
+app.post('/:id/update', function (req, res) {
 	var id = req.params.id;
 
 	categoryService.update(id, req.body, function (err, result) {
 		if (err) {
-			res.send(err); 
-		} else{
-			res.send(result); 
-		}				
+			res.send(err);
+		} else {
+			res.send(result);
+		}
 	});
 });
 
-app.get('/:id/products', function(req, res){
+app.get('/:id/products', function (req, res) {
 	var categoryId = req.params.id;
 
 	ProductService.findByCategory(categoryId, function (err, list) {
 		if (err) {
-			res.send(err); 
-		} else{
-			res.send(list); 
-		}				
+			res.send(err);
+		} else {
+			res.send(list);
+		}
 	});
 });
 
-app.put('/create', function(req, res){
-	
+app.put('/create', function (req, res) {
+
 	categoryService.create(req.body, function (err, result) {
 		if (err) {
-			res.send(err); 
-		} else{
-			res.send(result); 
-		}				
+			res.send(err);
+		} else {
+			res.send(result);
+		}
 	});
 });
 
--- Model/db.js
+++ Model/db.js
@@ -6,4 +6,4 @@ var mongoose = require('mongoose');
 
 //mongoose.connect('mongodb+srv://mongodb-stitch-panarkiaapi-rrppt:KvL-7sc-LGw-sWd@panarkia-detqs.gcp.mongodb.net/test?retryWrites=true');
 
-mongoose.connect('mongodb://panarkia:p4n4rk14@panarkia-shard-00-00-detqs.gcp.mongodb.net:27017,panarkia-shard-00-01-detqs.gcp.mongodb.net:27017,panarkia-shard-00-02-detqs.gcp.mongodb.net:27017/test?ssl=true&replicaSet=panarkia-shard-0&authSource=admin&retryWrites=true');
+// mongoose.connect('mongodb://panarkia:p4n4rk14@panarkia-shard-00-00-detqs.gcp.mongodb.net:27017,panarkia-shard-00-01-detqs.gcp.mongodb.net:27017,panarkia-shard-00-02-detqs.gcp.mongodb.net:27017/test?ssl=true&replicaSet=panarkia-shard-0&authSource=admin&retryWrites=true');
